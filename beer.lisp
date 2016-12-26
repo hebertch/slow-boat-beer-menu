06fd6315c1a695477cc0aa640a7fa27b1e1945b0
Christopher Hebert <hebert.christopherj@gmail.com>
2016-12-25 19:11:56 -0800

    all

--- beer.lisp
+++ beer.lisp
@@ -3,9 +3,16 @@
 (defparameter *row-height* 35)
 (defparameter *frame-top* -250.0)
 (defparameter *top* 0.0)
-(defparameter *left* 23)
+(defparameter *left* 23) 
 
-
+(defvar *beers*)
+(defvar *beers-csv*)
+(defvar *group-measures*)
+(defvar *font-sizes*)
+(defvar *offset-xs*)
+(defvar *offset-ys*)
+(defvar *rect-widths*)
+(defvar *rect-heights*)
 
 (ql:quickload :cl-ppcre)
 (ql:quickload :cl-ppcre-unicode)
@@ -57,6 +64,13 @@
     ("ch-desc" ,ch-desc)
     ("price" ,price)))
 
+(defun font-size-syms (ch-name en-name en-desc ch-desc price)
+  `(("ch-name-font-size" ,ch-name)
+    ("en-name-font-size" ,en-name)
+    ("en-desc-font-size" ,en-desc)
+    ("ch-desc-font-size" ,ch-desc)
+    ("price-font-size" ,price)))
+
 (defun process-stories (idx story-syms)
   (let* ((syms (append story-syms (story-id-syms idx))))
     (mapc (lambda (in-fname)
@@ -105,10 +119,30 @@
 
 (defun text-frame-syms-for-beer (id)
   (let ((y-off (* id *row-height*)))
-    (list (text-frame-syms (format nil "ch_name~A" (pad-num id)) (+ *left* 0) (+ *top* y-off 10.5) (+ *left* 161.5) (+ *top* y-off 22.5) 161.6)
-	  (text-frame-syms (format nil "en_name~A" (pad-num id)) (+ *left* 0) (+ *top* y-off .6) (+ *left* 210.7) (+ *top* y-off 22.3) 210.4)
-	  (text-frame-syms (format nil "desc~A" (pad-num id)) (+ *left* 0) (+ *top* y-off 18) (+ *left* 254) (+ *top* y-off 42) 254)
-	  (text-frame-syms (format nil "price~A" (pad-num id)) (+ *left* 220.6) (+ *top* y-off 0) (+ *left* 241.9) (+ *top* y-off 10.5) 21.3))))
+    (list (text-frame-syms (format nil "ch_name~A" (pad-num id))
+			   (+ *left* (cdr (assoc :ch-name *offset-xs*)))
+			   (+ y-off (cdr (assoc :ch-name *offset-ys*)))
+			   (+ *left* (cdr (assoc :ch-name *offset-xs*)) (cdr (assoc :ch-name *rect-widths*)))
+			   (+ y-off (cdr (assoc :ch-name *offset-ys*)) (cdr (assoc :ch-name *rect-heights*)))
+			   (+ (cdr (assoc :ch-name *offset-xs*)) (cdr (assoc :ch-name *rect-widths*))))
+	  (text-frame-syms (format nil "en_name~A" (pad-num id))
+			   (+ *left* (cdr (assoc :en-name *offset-xs*)))
+			   (+ y-off (cdr (assoc :en-name *offset-ys*)))
+			   (+ *left* (cdr (assoc :en-name *offset-xs*)) (cdr (assoc :en-name *rect-widths*)))
+			   (+ y-off (cdr (assoc :en-name *offset-ys*)) (cdr (assoc :en-name *rect-heights*)))
+			   (+ (cdr (assoc :en-name *offset-xs*)) (cdr (assoc :en-name *rect-widths*))))
+	  (text-frame-syms (format nil "desc~A" (pad-num id))
+			   (+ *left* (cdr (assoc :desc *offset-xs*)))
+			   (+ y-off (cdr (assoc :desc *offset-ys*)))
+			   (+ *left* (cdr (assoc :desc *offset-xs*)) (cdr (assoc :desc *rect-widths*)))
+			   (+ y-off (cdr (assoc :desc *offset-ys*)) (cdr (assoc :desc *rect-heights*)))
+			   (+ (cdr (assoc :desc *offset-xs*)) (cdr (assoc :desc *rect-widths*))))
+	  (text-frame-syms (format nil "price~A" (pad-num id))
+			   (+ *left* (cdr (assoc :price *offset-xs*)))
+			   (+ y-off (cdr (assoc :price *offset-ys*)))
+			   (+ *left* (cdr (assoc :price *offset-xs*)) (cdr (assoc :price *rect-widths*)))
+			   (+ y-off (cdr (assoc :price *offset-ys*)) (cdr (assoc :price *rect-heights*)))
+			   (+ (cdr (assoc :price *offset-xs*)) (cdr (assoc :price *rect-widths*)))))))
 
 (defun generate-text-frames-for-beer (id)
   (join-newline (mapcar (lambda (syms)
@@ -122,16 +156,40 @@
 						 collecting (generate-text-frames-for-beer i)))))))
     (process-template-file "drinks-template/Spreads/Spread_ub9.xml" "output/Spreads/Spread_ub9.xml" syms)))
 
-
-(defvar *beers*)
-
 (defun process-menu ()
   (setq *id* 0)
-  (setq *beers* (with-input-from-string (str (read-entire-file "beers.txt"))
-		  (cl-csv:read-csv str 
-				   :separator #\Tab
-				   :quote #\"
-				   :escape #\\)))
+
+  (setq *beers-csv* (with-input-from-string (str (read-entire-file "beers.txt"))
+		      (cl-csv:read-csv str 
+				       :separator #\Tab
+				       :quote #\"
+				       :escape #\\)))
+
+  (setq *group-measures* (pairlis
+			  '(:top :left :row-height)
+			  (mapcar 'read-from-string (subseq (first (cdr *beers-csv*)) 0 3))))
+  (setq *frame-top* (cdr (assoc :top *group-measures*))
+	*left* (cdr (assoc :left *group-measures*))
+	*row-height* (cdr (assoc :row-height *group-measures*)))
+
+  (setq *font-sizes* (pairlis
+		      '(:ch-name :en-name :en-desc :ch-desc :price)
+		      (mapcar 'read-from-string (cdr (cadddr *beers-csv*)))))
+
+  (setq *offset-xs* (pairlis
+		     '(:ch-name :en-name :desc :price)
+		     (mapcar 'read-from-string (subseq (nth 5 *beers-csv*) 1 5))))
+  (setq *offset-ys* (pairlis
+		     '(:ch-name :en-name :desc :price)
+		     (mapcar 'read-from-string (subseq (nth 6 *beers-csv*) 1 5))))
+  (setq *rect-widths* (pairlis
+		       '(:ch-name :en-name :desc :price)
+		       (mapcar 'read-from-string (subseq (nth 7 *beers-csv*) 1 5))))
+  (setq *rect-heights* (pairlis
+			'(:ch-name :en-name :desc :price)
+			(mapcar 'read-from-string (subseq (nth 8 *beers-csv*) 1 5))))
+
+  (setq *beers* (nthcdr 10 *beers-csv*))
 
   (let ((beers (mapcar 'butlast
 		       (remove-if-not (lambda (beer)
@@ -139,7 +197,12 @@
 				      *beers*))))
     (loop for i from 0
        for beer in beers
-       do (process-stories i (apply 'story-syms beer)))
+       do (process-stories i (append (apply 'story-syms beer)
+				     (font-size-syms (cdr (assoc :ch-name *font-sizes*))
+						     (cdr (assoc :en-name *font-sizes*))
+						     (cdr (assoc :en-desc *font-sizes*))
+						     (cdr (assoc :ch-desc *font-sizes*))
+						     (cdr (assoc :price *font-sizes*))))))
     (process-template-file "drinks-template/designmap.xml" "output/designmap.xml" (design-map-syms (length beers)))
     (process-spread-file (length beers))))
 
